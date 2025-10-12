87bfc304b1d63159066772d38a9cf7830463715b
Israelamat <israelamatgil@gmail.com>
2025-10-12 17:16:51 +0200

    Galleria_de_fotos

---
+++ Ejercicios_8.2_Adaptación_de_plantilla_2/templates/navegacion.part.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/../src/utils.class.php';
+?>
+<nav class="navbar navbar-fixed-top navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#menu">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand page-scroll" href="#page-top">
+        <span>[PHOTO]</span>
+      </a>
+    </div>
+    <div class="collapse navbar-collapse navbar-right" id="menu">
+      <ul class="nav navbar-nav">
+        <li class="<?php echo (utils::esOpcionMenuActiva('/index.php') || utils::esOpcionMenuActiva('/')) ? 'active lien' : 'lien'; ?>">
+          <a href="/templates/index.php"><i class="fa fa-home sr-icons"></i> Home</a>
+        </li>
+        <li class="<?php echo utils::esOpcionMenuActiva('/about.php') ? 'active lien' : 'lien'; ?>">
+          <a href="/templates/about.php"><i class="fa fa-bookmark sr-icons"></i> About</a>
+        </li>
+        <li class="<?php echo utils::esOpcionMenuActiva('/blog.php') ? 'active lien' : 'lien'; ?>">
+          <a href="/templates/blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a>
+        </li>
+        <li class="<?php echo utils::esOpcionMenuActiva('/contact.php') ? 'active lien' : 'lien'; ?>">
+          <a href="/templates/contact.php"><i class="fa fa-phone-square sr-icons"></i> Contact</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ src/entity/imagen.class.php
@@ -0,0 +1,135 @@
+<?php
+class imagen{
+
+    const RUTA_IMAGENES_PORTFOLIO = '/public/images/index/portfolio/';
+    const RUTA_IMAGENES_GALERIA   = '/public/images/index/gallery/';
+    const RUTA_IMAGENES_CLIENTES  = '/public/images/clients/';
+
+    /**
+     * @var int
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $nombre;
+    /**
+     * @var string
+     */
+    private $descripcion;
+    /**
+     * @var string
+     */
+    private $categoria;
+    /**
+     * @var int
+     */
+    private $numVisualizaciones;
+    /**
+     * @var int
+     */
+    private $numLikes;
+    /**
+     * @var int
+     */
+    private $numDownloads;
+
+    public function __construct(
+        string $nombre,
+        string $descripcion,
+        string $categoria,
+        int $numVisualizaciones = 0,
+        int $numLikes = 0,
+        int $numDownloads = 0
+    ){
+        $this->id = 0;
+        $this->nombre = $nombre;
+        $this->descripcion = $descripcion;
+        $this->categoria = $categoria;
+        $this->numVisualizaciones = $numVisualizaciones;
+        $this->numLikes = $numLikes;
+        $this->numDownloads = $numDownloads;
+    }
+
+    // -------------------- GETTERS --------------------
+    public function getId(): int { return $this->id; }
+    public function getNombre(): string { return $this->nombre; }
+    public function getDescripcion(): string { return $this->descripcion; }
+    public function getCategoria(): string { return $this->categoria; }
+    public function getNumVisualizaciones(): int { return $this->numVisualizaciones; }
+    public function getNumLikes(): int { return $this->numLikes; }
+    public function getNumDownloads(): int { return $this->numDownloads; }
+
+    // -------------------- SETTERS --------------------
+    /**
+     * @param string $nombre
+     * @return Imagen
+     */
+    public function setNombre(string $nombre): Imagen {
+        $this->nombre = $nombre;
+        return $this;
+    }
+
+    /**
+     * @param string $descripcion
+     * @return Imagen
+     */
+    public function setDescripcion(string $descripcion): Imagen {
+        $this->descripcion = $descripcion;
+        return $this;
+    }
+
+    /**
+     * @param string $categoria
+     * @return Imagen
+     */
+    public function setCategoria(string $categoria): Imagen {
+        $this->categoria = $categoria;
+        return $this;
+    }
+
+    /**
+     * @param int $numVisualizaciones
+     * @return Imagen
+     */
+    public function setNumVisualizaciones(int $numVisualizaciones): Imagen {
+        $this->numVisualizaciones = $numVisualizaciones;
+        return $this;
+    }
+
+    /**
+     * @param int $numLikes
+     * @return Imagen
+     */
+    public function setNumLikes(int $numLikes): Imagen {
+        $this->numLikes = $numLikes;
+        return $this;
+    }
+
+    /**
+     * @param int $numDownloads
+     * @return Imagen
+     */
+    public function setNumDownloads(int $numDownloads): Imagen {
+        $this->numDownloads = $numDownloads;
+        return $this;
+    }
+
+    // -------------------- toString --------------------
+    public function __toString(): string {
+        return $this->descripcion;
+    }
+
+    // -------------------- MÉTODOS PARA OBTENER URL --------------------
+    public function getUrlPortfolio(): string {
+        return self::RUTA_IMAGENES_PORTFOLIO . $this->getNombre();
+    }
+
+    public function getUrlGaleria(): string {
+        return self::RUTA_IMAGENES_GALERIA . $this->getNombre();
+    }
+
+    public function getUrlClientes(): string {
+        return self::RUTA_IMAGENES_CLIENTES . $this->getNombre();
+    }
+}
